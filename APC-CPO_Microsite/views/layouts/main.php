82594e0d33887046822a7a91f0e7b2a0468f959d
Alyssa Mae Acle <amcacle101@gmail.com>
2015-02-10 14:47:59 +0800

    Committed

--- APC-CPO_Microsite/views/layouts/main.php
+++ APC-CPO_Microsite/views/layouts/main.php
@@ -32,6 +32,7 @@ AppAsset::register($this);
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
+			
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
@@ -47,7 +48,7 @@ AppAsset::register($this);
             ]);
             NavBar::end();
         ?>
-
+		
         <div class="container">
             <?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
