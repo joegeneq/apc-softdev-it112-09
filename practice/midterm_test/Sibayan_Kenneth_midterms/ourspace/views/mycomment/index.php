d56454bfd63cf7abd5310c0fb82a6dffa7f5ae70
Kenneth Sibayan <kensbyn@gmail.com>
2015-02-28 08:51:55 +0800

    generated crud for the tables

---
+++ practice/midterm_test/Sibayan_Kenneth_midterms/ourspace/views/mycomment/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\MyCommentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'My Comments';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="my-comment-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create My Comment', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'myaddress_id',
+            'author',
+            'body:ntext',
+            'created_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
