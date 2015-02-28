402736b975d1cc5a3eac3d8a46732766f924bd53
Kenneth Sibayan <kensbyn@gmail.com>
2015-03-01 02:59:54 +0800

    [MIDTERM] Continued to letter M: updated list and view in mycomment

--- practice/midterm_test/Sibayan_Kenneth_midterms/ourspace/views/mycomment/index.php
+++ practice/midterm_test/Sibayan_Kenneth_midterms/ourspace/views/mycomment/index.php
@@ -26,7 +26,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'myaddress_id',
+            'myaddress.lastname',
             'author',
             'body:ntext',
             'created_at',
