2b95968300200772ec6cf6923ba91fb45a0ee003
josh <josh.ramos10@gmail.com>
2015-03-01 14:40:02 +0800

    Modified config files to connect to softdev_ramos_josiah_immanuel_ourspace

--- practice/midterm_test/Ramos_Josiah_Immanuel_midterms/ourspace/config/db.php
+++ practice/midterm_test/Ramos_Josiah_Immanuel_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_ramos_josiah_immanuel_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
