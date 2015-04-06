ffed75432cd4fcdf9e4ea175f6b142446b41f92c
Josiah Immanuel R. Ramos <josh.ramos10@gmail.com>
2015-04-06 13:41:06 +0800

    Rerouted system pictures and background files Error redirection

--- application/frontend/views/site/about.php
+++ application/frontend/views/site/about.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<body background="images/bg.jpg">
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
