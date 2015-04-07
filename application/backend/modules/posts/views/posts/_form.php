fa44f0c06c2d185127179b670d00bac0f67ce182
Ken Sibayan <kensbyn@gmail.com>
2015-04-07 09:14:23 +0800

    editing posts feature of the site
    customizing views

--- application/backend/modules/posts/views/posts/_form.php
+++ application/backend/modules/posts/views/posts/_form.php
@@ -10,17 +10,19 @@ use yii\widgets\ActiveForm;
 
 <div class="posts-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
     <?= $form->field($model, 'posts_title')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'posts_body')->textarea(['rows' => 6]) ?>
 
+    <?= $form->field($model, 'file')->fileInput(); ?>
+
     <?= $form->field($model, 'author')->textInput() ?>
 
     <?= $form->field($model, 'author_role')->textInput() ?>
 
-    <?= $form->field($model, 'post_type')->textInput() ?>
+    <?= $form->field($model, 'post_type')->dropDownList(['Announcements','Internship Openings','Reminders','HR Request'], ['prompt'=>'Select...']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
