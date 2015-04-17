5a31cbc5c28c40f9e1bb7ea2e9b30bd573615c98
Kenneth Sibayan <kensbyn@gmail.com>
2015-04-17 14:40:15 +0800

    removed registration notification; edited posts view
    removed registration notification as it produces connection error
    rbac display of buttons in posts view

--- application/frontend/views/posts/view.php
+++ application/frontend/views/posts/view.php
@@ -20,14 +20,21 @@ $this->params['breadcrumbs'][] = $this->title;?>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?php
+        if(Yii::$app->user->isGuest==false){
+            if(Yii::$app->user->identity->roles == 25){
+
+            Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
+            Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
-        ]) ?>
+        ]); 
+            }
+        }
+        ?>
     </p>
 <?php
         function role($data){
