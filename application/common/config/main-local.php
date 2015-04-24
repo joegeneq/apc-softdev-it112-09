35a35da1e49dd6f5c4f84aa6264a72e4fbc2be28
Kenneth Sibayan <kensbyn@gmail.com>
2015-04-24 13:20:50 +0800

    put urlManager to common config

--- application/common/config/main-local.php
+++ application/common/config/main-local.php
@@ -22,6 +22,19 @@ return [
             'encryption' => 'tls',
                         ],
         ],
+
+         'urlManager' => [
+        'class' => 'yii\web\UrlManager',
+        // Disable index.php
+        'showScriptName' => false,
+        // Disable r= routes
+        'enablePrettyUrl' => true,
+        'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+        ),
+        ],
         ],
 ];
 
